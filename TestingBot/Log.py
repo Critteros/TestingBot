a278203e0e3d41e97ed3f0c53feaf882854db445
Critteros <szymonkoziol3@gmail.com>
2020-12-27 22:03:06 +0100

    Finished Bot Logger class

--- TestingBot/Log.py
+++ TestingBot/Log.py
@@ -4,8 +4,9 @@
 #     CONSOLE /This will set the logger to use the coloredlogs library and print colored logs to STDOUT
 #     NORMAL  /This will set the logger to use the logging module and write output to STDOUT
 #     FILE    /This will set the logger to use the logging module and write output to a file
+#           Default is NORMAL
 #
-#LOG_LEVEL
+# LOG_LEVEL
 #   DEBUG /Sets log level to debug
 #   INFO /Sets log level to info
 #   WARNING /Sets log level to warning
@@ -13,7 +14,7 @@
 #   CRITICAL /Sets log level to critical
 #
 # LOG_INTERNAL
-#   setting this enviroment variable will enable discord.py library to log to a file
+#   if set to TRUE  this setting enviroment variable will enable discord.py library to log to a file
 
 # Standard imports
 import sys
@@ -31,28 +32,108 @@ if (__name__ == "__main__"):
 
 class BotLog:
 
+    #Prevent instance creation of this class
+    def __new__(cls, *args, **kwargs):
+        print("FATAL: Creating instances of main bot class is not supported")
+        raise RuntimeError("BotLog instance creation: forbidden")
+    
     # Class variables to be used
     _clientLogger = None
     _internalLogger = None
 
-    @classmethod
-    def init(cls, *, level=logging.DEBUG):
-        cls._initConsoleLogger(level=level)
-        cls._initInternalLogger(level=level)
         
-        #Testing only
+    #To test logger functionality
+    @classmethod
+    def logExamples(cls):
         cls._clientLogger.debug("Example debug")
         cls._clientLogger.info("Example info")
         cls._clientLogger.warning("Example warning")
-        cls._clientLogger.debug("Finieshed intialization of Logger")
         cls._clientLogger.error("Example error")
         cls._clientLogger.critical("Example critical")
-
-    # ConsoleLogger getter
+    
+    #ConsoleLogger getter
     @classmethod
     def getConsoleLogger(cls):
         return cls._clientLogger
 
+####################################################################################################
+#                       LOGGER BYPASSES
+####################################################################################################
+    @classmethod
+    def debug(cls,*args, **kwargs):
+        cls._clientLogger.debug(*args, **kwargs)
+        
+    @classmethod
+    def info(cls,*args, **kwargs):
+        cls._clientLogger.info(*args, **kwargs)
+        
+    @classmethod
+    def warning(cls,*args, **kwargs):
+        cls._clientLogger.warning(*args, **kwargs)
+        
+    @classmethod
+    def error(cls,*args, **kwargs):
+        cls._clientLogger.error(*args, **kwargs)
+        
+    @classmethod
+    def critical(cls,*args, **kwargs):
+        cls._clientLogger.critical(*args, **kwargs)
+    
+####################################################################################################
+
+    #Inner init method, do not use it
+    @classmethod
+    def _init(cls, *, level = None):
+        
+        #Defaults
+        LEVEL_VALUES = {
+            "DEBUG": logging.DEBUG,
+            "INFO" : logging.INFO,
+            "WARNING": logging.WARNING,
+            "ERROR" : logging.ERROR,
+            "CRITICAL": logging.CRITICAL
+        }
+        
+        CLIENT_VALUES = {
+            "CONSOLE",
+            "NORMAL",
+            "FILE"
+        }
+        
+        #Read env variables
+        level = level or os.environ.get('LOG_LEVEL', 'WARNING')
+        client_type = os.environ.get('CLIENT_LOGGER', 'NORMAL')
+        
+        #Handle internal env
+        tmp = os.getenv('LOG_INTERNAL')
+        if ( (tmp is not None) and (tmp == "TRUE")):
+            initInternal = True
+        else:
+            initInternal = False
+        del tmp
+        
+        #User input handling
+        level = level if (level in LEVEL_VALUES) else "WARNING"
+        client_type = client_type if (client_type in CLIENT_VALUES) else "NORMAL"
+        
+        #Set level to a int variable
+        level = LEVEL_VALUES[level]
+        
+        if client_type == "CONSOLE":
+            cls._initConsoleLogger(level=level)
+        elif client_type == "NORMAL":
+            cls._initNormalLogger(level=level)
+        elif client_type == "FILE":
+            cls._initFileLogger(level=level)
+        else:
+            cls._initNormalLogger(level=level)
+            
+        if initInternal:
+            cls._initInternalLogger(level=level)
+            
+####################################################################################################
+
+    #Inner function to init Console Logger
     @classmethod
     def _initConsoleLogger(cls, *, level=logging.DEBUG):
         cls._clientLogger = logging.getLogger("TestingBot")
@@ -99,10 +180,14 @@ class BotLog:
             logger=cls._clientLogger,
             stream=Stout
         )
-    
+
+####################################################################################################    
+
+    #Inner function to init File Logger
     @classmethod
     def _initFileLogger(cls,*,level=logging.DEBUG):
         cls._clientLogger = logging.getLogger("TestingBot")
+        cls._clientLogger.setLevel(level)
 
         # Formatted logger output
         fmt = "%(asctime)s | %(name)s[%(process)d] %(levelname)s: %(message)s"
@@ -119,10 +204,14 @@ class BotLog:
         
         #Attach handler
         cls._clientLogger.addHandler(handler)
-        
+
+####################################################################################################
+
+    #Inner function to init Normal logger    
     @classmethod
     def _initNormalLogger(cls,*,level=logging.DEBUG):
         cls._clientLogger = logging.getLogger("TestingBot")
+        cls._clientLogger.setLevel(level)
         
         # Getting hook to stdout stream
         Stout = sys.stdout
@@ -142,7 +231,10 @@ class BotLog:
         
         #Attach handler
         cls._clientLogger.addHandler(handler)
-        
+      
+####################################################################################################    
+    
+    #Inner funtion to init Internal logger  
     @classmethod
     def _initInternalLogger(cls,*,level=logging.DEBUG):
         cls._internalLogger = logging.getLogger("discord")
@@ -163,8 +255,10 @@ class BotLog:
         #Attach handler
         cls._internalLogger.addHandler(handler)
 
+####################################################################################################
 
 # Run code upon import
-if (__name__ == "TestingBot.Log"):
+if (__name__ != "__main__"):
     # Initialize Loggers
-    BotLog.init()
+    BotLog._init()
+    BotLog.debug("Finished initializing BotLogger class")
